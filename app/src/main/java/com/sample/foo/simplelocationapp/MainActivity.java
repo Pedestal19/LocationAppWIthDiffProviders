cb9687785118e2d0a7a77ff2408671e21b10c5a5
HosannaGabeOji <gabeoji01@gmail.com>
2019-10-04 14:13:30 +0100

    calc distance : get points for every 10m : init points after 10m -- append values in newly created textview

--- app/src/main/java/com/sample/foo/simplelocationapp/MainActivity.java
+++ app/src/main/java/com/sample/foo/simplelocationapp/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         else {
             locationManager.requestLocationUpdates(
 //                    LocationManager.GPS_PROVIDER, 30 * 60 * 1000, 5, locationListenerGPS);
-                    LocationManager.GPS_PROVIDER, 0, 0, locationListenerGPS);
+                    LocationManager.GPS_PROVIDER, 3*1000, 0, locationListenerGPS);
 
             button.setText(R.string.pause);
         }
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
             String provider = locationManager.getBestProvider(criteria, true);
             if(provider != null) {
 //                locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10, locationListenerBest);
-                locationManager.requestLocationUpdates(provider, 0, 10, locationListenerBest);
+                locationManager.requestLocationUpdates(provider, 3*1000, 0, locationListenerBest);
 
                 button.setText(R.string.pause);
                 Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
@@ -158,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+                    LocationManager.NETWORK_PROVIDER, 3*1000, 0, locationListenerNetwork);
             Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
             button.setText(R.string.pause);
         }
@@ -167,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
     private final LocationListener locationListenerBest = new LocationListener() {
 
         public void onLocationChanged(Location location) {
-            Log.w(TAG, "@@@@@@@@@you hae moved more than 10 m");
+//            Log.w(TAG, "@@@@@@@@@you hae moved more than 30 m");
 
             longitudeBest = location.getLongitude();
             latitudeBest = location.getLatitude();
@@ -188,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            float theDistance = distance(oldLatitudeBest,oldLongitudeBest,latitudeBest, longitudeBest);
+            double theDistance = distanceBetweenTwoPoint(oldLatitudeBest,oldLongitudeBest,latitudeBest, longitudeBest);
             Log.e(TAG, "current calc of the distance is "+theDistance);
 
             tv_distance.append("\n current calc of the distance is"+theDistance);
@@ -424,6 +424,21 @@ public class MainActivity extends AppCompatActivity {
         return distance;
     }
 
+    double distanceBetweenTwoPoint(double srcLat, double srcLng, double desLat, double desLng) {
+        double earthRadius = 3958.75;
+        double dLat = Math.toRadians(desLat - srcLat);
+        double dLng = Math.toRadians(desLng - srcLng);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(srcLat))
+                * Math.cos(Math.toRadians(desLat)) * Math.sin(dLng / 2)
+                * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius * c;
+
+        double meterConversion = 1609;
+
+        return (int) (dist * meterConversion);
+    }
 
 
 }
